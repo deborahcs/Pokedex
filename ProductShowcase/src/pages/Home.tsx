d93bbced95935867e1541f494376ba53c49ad5d5
Deborah Cabral <deborahcabralsilva@gmail.com>
2026-07-09 16:37:53 -0300

    refactor: move pokemon interface to types folder

--- ProductShowcase/src/pages/Home.tsx
+++ ProductShowcase/src/pages/Home.tsx
@@ -1,8 +1,30 @@
-// Componente principal  que renderiza a lista de Pokemon da Api
+import { useState, useEffect } from 'react';
+import { api } from '../services/api';
+import type { Pokemon } from '../types/pokemon';
+
+// Componente principal que renderiza a lista de Pokemons da API
 export function Home() {
+  // Cria o estado para armazenar a lista de pokemons
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  // Executa a busca na API assim que o componente e montado na tela
+  useEffect(() => {
+    async function fetchPokemons() {
+      const response = await api.get('/pokemon');
+      setPokemons(response.data.results);
+    }
+
+    fetchPokemons();
+  }, []);
+
   return (
     <div>
-      <h1>Página Home - Lista de Pokémons</h1>
+      <h1>Página Home Lista de Pokémons</h1>
+      <ul>
+        {pokemons.map((pokemon, index) => (
+          <li key={index}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ ProductShowcase/src/types/pokemon.ts
@@ -0,0 +1,5 @@
+// Exportamos o cracha para que qualquer pagina possa usar
+export interface Pokemon {
+  name: string;
+  url: string;
+}
